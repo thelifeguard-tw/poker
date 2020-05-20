ead612195a43b4096c9a7d4823441f19e42cb574
panachaireintw <panachai.r@thoughtworks.com>
2020-05-20 16:38:36 +0700

    init server from scratch with typescript-ready

--- packages/server/src/Server.ts
+++ packages/server/src/Server.ts
@@ -1,69 +1,11 @@
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import helmet from 'helmet';
+import * as express from "express";
 
-import express, { Request, Response, NextFunction } from 'express';
-import { BAD_REQUEST, OK } from 'http-status-codes';
-import 'express-async-errors';
+const app = express.default();
 
-import BaseRouter from './routes';
-import logger from '@shared/Logger';
+const port = 3000;
 
+app.get("/", (req, res) => res.send("Hello World!"));
 
-// Init express
-const app = express();
-
-
-
-/************************************************************************************
- *                              Set basic express settings
- ***********************************************************************************/
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-
-// Show routes called in console during development
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
-
-// Security
-if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
-}
-
-// Add APIs
-app.use('/api', BaseRouter);
-
-// Add healthcheck
-app.get('/healthcheck', (req: Request, res: Response) => {
-  logger.info('the server is fine.')
-  return res.status(OK).json({status:'OK'});
-});
-
-// Print API errors
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message, err);
-    return res.status(BAD_REQUEST).json({
-        error: err.message,
-    });
-});
-
-
-
-/************************************************************************************
- *                              Serve front-end content
- ***********************************************************************************/
-
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
-});
-
-// Export express instance
-export default app;
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
