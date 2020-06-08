4624a35320169782236e041bc18224f6e157076a
panachaireintw <panachai.r@thoughtworks.com>
2020-06-08 14:40:05 +0700

    [mob] set up terraform cloud to keep state remotely

--- main.tf
+++ main.tf
@@ -1,12 +1,24 @@
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "tw-lifeguard"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "tw-lifeguard"
+    }
+  }
+}
+
 provider "aws" {
   version = "~> 2.0"
   region  = "ap-southeast-1"
 }
 
-# resource "aws_instance" "test_box" {
-#   ami           = "ami-0615132a0f36d24f4"
-#   instance_type = "t2.micro"
-# }
+resource "aws_instance" "lifeguard-prod" {
+  ami           = "ami-0615132a0f36d24f4"
+  instance_type = "t2.micro"
+}
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "tw-poker-terraform-state-prod-please-be-uniq"
