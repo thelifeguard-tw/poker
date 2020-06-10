3009cf3a866c1ecb853e9f2155d288b10142f8e4
IQ <santiphap.w@thoughtworks.com>
2020-06-10 14:24:04 +0700

    [Mob] Add AWS EKS in Terraform

--- main.tf
+++ main.tf
@@ -37,11 +37,53 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-# data "terraform_remote_state" "network" {
-#   backend = "s3"
-#   config = {
-#     bucket = "terraform-state-prod"
-#     key    = "poker/terraform.tfstate"
-#     region = "ap-southeast-1"
-#   }
-# }
+resource "aws_eks_cluster" "default" {
+  name     = "default"
+  role_arn = "${aws_iam_role.example.arn}"
+
+  # vpc_config {
+  #   subnet_ids = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
+  # }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    "aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.example-AmazonEKSServicePolicy",
+  ]
+}
+output "endpoint" {
+  value = "${aws_eks_cluster.example.endpoint}"
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = "${aws_eks_cluster.example.certificate_authority.0.data}"
+}
+resource "aws_iam_role" "example" {
+  name = "eks-cluster-example"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.example.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.example.name}"
+}
