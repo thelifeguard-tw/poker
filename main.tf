97d25dfa624529df0b46eca6d3340e7c0bf598cd
IQ <santiphap.w@thoughtworks.com>
2020-06-10 14:54:51 +0700

    [Mob] Fix availability zone

--- main.tf
+++ main.tf
@@ -43,12 +43,12 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "example1" {
   vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1"
+  availability_zone = "ap-southeast-1a"
 }
 resource "aws_subnet" "example2" {
   vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-2"
+  availability_zone = "ap-southeast-1b"
 }
 
 resource "aws_eks_cluster" "default" {
