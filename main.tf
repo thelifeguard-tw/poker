0e9e508f9421a334eb967d45b59c542f2a6b2ae1
thank narerkrit <narerkrit.s@thoughtworks.com>
2020-06-11 14:18:07 +0700

    [mob] remove unused terraform resources

--- main.tf
+++ main.tf
@@ -20,36 +20,36 @@ resource "aws_instance" "lifeguard-prod" {
   instance_type = "t2.micro"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tw-poker-terraform-state-prod-please-be-uniq"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "tw-poker-terraform-state-prod-please-be-uniq"
+#   # Enable versioning so we can see the full revision history of our
+#   # state files
+#   versioning {
+#     enabled = true
+#   }
+#   # Enable server-side encryption by default
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-resource "aws_subnet" "example1" {
-  vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1a"
-}
-resource "aws_subnet" "example2" {
-  vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-1b"
-}
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+# }
+# resource "aws_subnet" "example1" {
+#   vpc_id            = "${aws_vpc.main.id}"
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = "ap-southeast-1a"
+# }
+# resource "aws_subnet" "example2" {
+#   vpc_id            = "${aws_vpc.main.id}"
+#   cidr_block        = "10.0.2.0/24"
+#   availability_zone = "ap-southeast-1b"
+# }
 
 # resource "aws_eks_cluster" "default" {
 #   name     = "default"
@@ -73,31 +73,31 @@ resource "aws_subnet" "example2" {
 # output "kubeconfig-certificate-authority-data" {
 #   value = "${aws_eks_cluster.default.certificate_authority.0.data}"
 # }
-resource "aws_iam_role" "example" {
-  name = "eks-cluster-example"
+# resource "aws_iam_role" "example" {
+#   name = "eks-cluster-example"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
+#   assume_role_policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "eks.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.example.name}"
-}
+# resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#   role       = "${aws_iam_role.example.name}"
+# }
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.example.name}"
-}
+# resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+#   role       = "${aws_iam_role.example.name}"
+# }
