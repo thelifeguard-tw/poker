633a3ec2b94685fefbed1002451fa7df196b5a44
thank narerkrit <narerkrit.s@thoughtworks.com>
2020-06-15 15:12:55 +0700

    [mob] try adding private key for provisioning

--- main.tf
+++ main.tf
@@ -19,7 +19,9 @@ resource "aws_instance" "lifeguard-prod" {
   ami             = "ami-0615132a0f36d24f4"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.secure-shell-ec2.name]
-  # key_name        = "test1"
+  key_name        = "test1"
+
+
 
   provisioner "remote-exec" {
     inline = [
@@ -27,12 +29,11 @@ resource "aws_instance" "lifeguard-prod" {
       "sudo apt-get install curl docker openjdk-8-jdk",
       "sudo curl -o batect https://github.com/batect/batect/releases/download/0.53.1/batect"
     ]
-
     connection {
-      type = "ssh"
-      host = aws_instance.lifeguard-prod.public_ip
-      user = "ec2-user"
-      port = 22
+      type        = "ssh"
+      host        = aws_instance.lifeguard-prod.public_ip
+      user        = "ec2-user"
+      private_key = file("ssh/key.pem")
     }
   }
 }
