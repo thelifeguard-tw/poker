7c698fe6313644ac9f88d543741c227882b11f81
IQ <santiphap.w@thoughtworks.com>
2020-06-16 13:19:13 +0700

    [Mob] Add outbound rule for security groups

--- main.tf
+++ main.tf
@@ -48,6 +48,14 @@ resource "aws_security_group" "secure-shell-ec2" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "ssh"
   }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
+  }
 }
 
 
