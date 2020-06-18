9d1e6b52f2faea5e46ed9e6887390992accd30db
thank narerkrit <narerkrit.s@thoughtworks.com>
2020-06-18 14:59:50 +0700

    [mob] fix sudo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker run --rm -d -p 80:3000 twlifeguard/poker-client
-docker run --rm -d -p 5000:5000 twlifeguard/poker-server
+sudo docker run --rm -d -p 80:3000 twlifeguard/poker-client
+sudo docker run --rm -d -p 5000:5000 twlifeguard/poker-server
